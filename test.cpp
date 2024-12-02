f422aa8ab5e01644ed11ea5b5e630936c36ea1d9
Choun Rathanak <Nak4131@gmail.com>
2024-12-02 23:05:50 +0700

    test

--- test.cpp
+++ test.cpp
@@ -8,13 +8,13 @@ int main(){
     ArrayList<User> user;
     User tempUser;
     
-    tempUser.input(USER_FILE);
+  /*  tempUser.input(USER_FILE);
     user.push(tempUser);
     writeToBinary(USER_FILE,user);
 
     tempUser.input(USER_FILE);
     user.push(tempUser);
-    writeToBinary(USER_FILE,user);
+   writeToBinary(USER_FILE,user);*/
 
     readFromBinary(USER_FILE,user);
     for(int i=0;i<user.getLength();i++){
