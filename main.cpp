01c69c03ea6f223722f2b29bf63d6e2b0cf7d4e5
ChanpanhaCheng <chanpanhacheng@gmail.com>
2024-12-02 23:06:08 +0700

    test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,31 @@
 #include "./utils/Bank.hpp"
 #include "./utils/ATM.hpp"
 int main(){
+   bool out=true;
+   int choice;
+   
+   do
+   {
+      cout<<"Welcome to our bank !"<<endl;
+      cout<<"Press the number to enter!"<<endl;
+      cout<<"1./ User "<<endl;
+      cout<<"2./ Admin"<<endl;
+      cout<<"3./ ATM"<<endl;
+      cout<<"Your choice : ";cin>>choice;
+      switch (choice)
+      {
+      case 1:
+         
+         break;
+      
+      default:
+      cout<<
+         break;
+      }
+
+      
+   } while (out);
+   
    
    return 0;
 }
