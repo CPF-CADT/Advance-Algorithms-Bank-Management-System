418b2a24b2bd43639d23a33f9ad8d0d9a9429850
Chhun Sivheng <sivmiosu@gmail.com>
2024-12-12 21:19:15 +0700

    test1

--- Learn/test.cpp
+++ Learn/test.cpp
@@ -1,10 +1,10 @@
 #include"../utils/User.hpp"
 int main(){
     ArrayList<User> user;
-    // readFromCV("../Data/custom_generated_data.csv",user,"../Data/usr.dat");
-    readFromBinary("../Data/usr.dat",user);
-    for(int i=0;i<user.getLength();i++){
-        user.getValue(i).displayInfo();
-    }
+    readFromCV("../Data/custom_generated_data.csv",user,"../Data/usr.dat");
+    // readFromBinary("../Data/usr.dat",user);
+    // for(int i=0;i<user.getLength();i++){
+    //     user.getValue(i).displayInfo();
+    // }
     return 0;
 }
--- utils/ATM.hpp
+++ utils/ATM.hpp
@@ -26,4 +26,3 @@ class ATM {
             balance = balance + amount;
         }
 };
-#endif
