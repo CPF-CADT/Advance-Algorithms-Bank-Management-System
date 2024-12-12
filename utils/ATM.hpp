9c745e186be821403aafe45af8f242459421a30c
Chhun Sivheng <sivmiosu@gmail.com>
2024-12-12 08:49:53 +0700

    ATM

--- utils/ATM.hpp
+++ utils/ATM.hpp
@@ -1,1 +1,25 @@
-_GLIBCXX_HAVE_STRING_H
+#ifndef ATM.HPP
+#define ATM.HPP
+#include<vector>
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+    class ATM{
+        private:
+        vector<User> users;
+    }
+        public:
+            AMT(){
+                users.pushback(User("085825783"), 1000);
+            }
+            User* searchUser(const string& phoneNumber){
+                for(auto& user : users ){
+                    if ( user.getPhoneNumber() == phoneNumber){
+                        return &user;
+                    }
+                }
+                return nullptr;
+            }
+
