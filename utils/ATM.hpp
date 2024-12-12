c5596c0fb13c60f075b4e55a88bbc0d4667b5c38
Chhun Sivheng <sivmiosu@gmail.com>
2024-12-12 11:19:19 +0700

    changed

--- utils/ATM.hpp
+++ utils/ATM.hpp
@@ -1,27 +1,32 @@
 #ifndef ATM.HPP
 #define ATM.HPP
-#include<vector>
-#include<iostream>
-#include<string>
+
 #include "User.hpp"
+#include <string>
+#include<iostream>
+
 using namespace std;
 
-    class ATM{
-        private:
-        vector<User> users;
-    }
-        public:
-            AMT(){
-                users.pushback(User("085825783"), 1000);
+class ATM {
+    private:
+        int balance;
+    public:
+        ATM(int bal){
+            balance = bal;
+        }
+        int getBalance(){
+            return balance;
+        }
+        int withdraw(int amount){
+            if(amount > balance ){
+                return false;
+            } else {
+                balance = balance - amount;
+                    return true;
             }
-            User* searchUser(const string& phoneNumber){
-                for(auto& user : users ){
-                    if ( user.getPhoneNumber() == phoneNumber){
-                        return &user;
-                    }
-                }
-                return nullptr;
-            }
-            void withdraw(User& user){
-                cout<<"Your current balance : $" << User.getbalance<<
-            }
+        }
+        int deposite(int amount){
+            balance = balance + amount;
+        }
+};
+#endif
